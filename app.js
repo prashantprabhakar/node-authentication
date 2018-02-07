7835cf86ec377259bbd19524e868c723f584854f
prashant.prabhakar <prashant.prabhakar@sofocle.com>
2018-02-07 15:06:20 +0530

    Node-auth using passport

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var app = express();
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -7,6 +8,7 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 const passport = require('passport')
 const session = require('express-session')
+var flash    = require('connect-flash');
 //const RedisStore = require('connect-redis')(session)
 
 
@@ -14,42 +16,47 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 var config = require('./config/config.js');
 
+require('./config/passport')(passport);
+
 var dbUrl = config.database;
 
 
-var app = express();
+// Configure express app ===================================
 
-/*app.use(session({
-  store: new RedisStore({
-    url: config.redisStore.url
-  }),
-  secret: config.redisStore.secret,
-  resave: false,
-  saveUninitialized: false
-}));*/
-app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
-
-
-app.use(passport.initialize())
-app.use(passport.session())
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+	/*app.use(session({
+	  store: new RedisStore({
+	    url: config.redisStore.url
+	  }),
+	  secret: config.redisStore.secret,
+	  resave: false,
+	  saveUninitialized: false
+	}));*/
+	app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
 
 
-app.use('/', index);
-app.use('/users', users);
+	app.use(passport.initialize())
+	app.use(passport.session())
+
+	// view engine setup
+	app.set('views', path.join(__dirname, 'views'));
+	app.set('view engine', 'ejs');
+
+	// uncomment after placing your favicon in /public
+	//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+	app.use(logger('dev'));
+	app.use(bodyParser.json());
+	app.use(bodyParser.urlencoded({ extended: false }));
+	app.use(cookieParser());
+	app.use(express.static(path.join(__dirname, 'public')));
+	app.use(flash()); // use connect-flash for flash messages stored in session
+
+
+
+
+// routes ==============================
+
+require('./routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
 
 
 /*Connect to database*/
@@ -60,7 +67,6 @@ mongoose.connection.on('error', function(err){
 	process.exit(0);
 });
 
-
 mongoose.connection.once('open', function() {
 	//Lets start our server
 	app.listen(config.serverport, function() {
@@ -70,7 +76,7 @@ mongoose.connection.once('open', function() {
 
 
 
-
+/*
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -87,7 +93,8 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
-});
+  	res.render('error');
+	});
+*/
 
 module.exports = app;
